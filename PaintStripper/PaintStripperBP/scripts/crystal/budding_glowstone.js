9a5b80238ddf3b5c53bea2729cd4a39695b3e312
Beardedflea <Linzell.Godfrey@live.co.uk>
2024-10-14 19:46:32 +0100

    seed crystal recipes and refactored scripts

---
+++ PaintStripper/PaintStripperBP/scripts/crystal/budding_glowstone.js
@@ -0,0 +1,73 @@
+import {world, system, ItemStack } from "@minecraft/server";
+
+world.beforeEvents.worldInitialize.subscribe(eventData => {
+    eventData.blockComponentRegistry.registerCustomComponent('ps:ort_bud_growth', {
+        onRandomTick(e) {
+            const { block } = e;
+            if(Math.floor(Math.random() * 100) > 20) return;
+            
+            const blockdAndFaces = [{block:block.above(), face:"up"},{block:block.below(), face:"down"},{block:block.north(), face:"north"}, 
+                                    {block:block.south(), face:"south"},{block:block.west(), face:"west"},{block:block.east(), face:"east"},]
+            //ive used "block" enough times. Lets spice things up                        
+            const availableLumps = blockdAndFaces.filter(parallelepiped => 
+                                                            parallelepiped.block.isAir || parallelepiped.block.typeId === "ps:cluster_glowstone");
+
+            if(availableLumps.length === 0) return;
+
+            const budToGrow = availableLumps[Math.floor(Math.random() * availableLumps.length)]
+            
+            if(budToGrow.block.isAir){
+                budToGrow.block.setType("ps:cluster_glowstone")
+                budToGrow.block.setPermutation(budToGrow.block.permutation.withState("minecraft:block_face", budToGrow.face)); 
+            }
+            else{
+                const seedStage = budToGrow.block.permutation.getState('ps:crystal_stage');
+                if(seedStage === 4) return;
+                budToGrow.block.setPermutation(budToGrow.block.permutation.withState('ps:crystal_stage', seedStage+1)); 
+            }
+        }
+    });
+});
+
+world.afterEvents.playerBreakBlock.subscribe((e) => {
+    const { brokenBlockPermutation, dimension,block } = e;
+
+    if (brokenBlockPermutation.type.id === "minecraft:glowstone" && (Math.floor(Math.random() * 100) < 5)) {
+        dimension.spawnItem(new ItemStack("ps:glowstone_shard", 1), block.location);
+    }
+});
+
+world.beforeEvents.worldInitialize.subscribe(eventData => {
+    eventData.blockComponentRegistry.registerCustomComponent('ps:bop_cluster_glowstone', {
+        beforeOnPlayerPlace(e) {
+            const { player, permutationToPlace } = e;
+            // const affectedBlock = getBlockFromFace(block, face)
+            // if(!affectedBlock.isSolid){
+            //     e.cancel = true;
+            //     return;
+            // }
+            const equipment = player.getComponent('equippable');
+            const selectedItem = equipment.getEquipment('Mainhand');
+            let stageSize; 
+            
+            if(selectedItem.typeId === "ps:glowstone_cluster"){
+                stageSize = 4;
+            }else{
+                const clusterSize = selectedItem.typeId.split("_")[0].substring(3);
+
+                switch(clusterSize){
+                    case "large":
+                        stageSize = 3;
+                    break;
+                    case "medium":
+                        stageSize = 2;
+                    break;
+                    case "small":
+                        stageSize = 1;
+                    break;
+                }
+            }
+            e.permutationToPlace = permutationToPlace.withState('ps:crystal_stage', stageSize);
+            }
+    });
+});
--- PaintStripper/PaintStripperBP/scripts/main.js
+++ PaintStripper/PaintStripperBP/scripts/main.js
@@ -1,6 +1,6 @@
 import {world, system, ItemStack } from "@minecraft/server";
 import {getAdjacentBlock, getBlockFromFace} from "blockPlacementUtils.js";
-
+import "./crystal/budding_glowstone.js"
 world.beforeEvents.worldInitialize.subscribe(eventData => {
     eventData.blockComponentRegistry.registerCustomComponent('ps:seed_test', {
         onPlayerInteract(e) {
@@ -22,78 +22,25 @@ world.beforeEvents.worldInitialize.subscribe(eventData => {
     });
 });
 
-world.beforeEvents.worldInitialize.subscribe(eventData => {
-    eventData.blockComponentRegistry.registerCustomComponent('ps:on_random_tick_bud_growth', {
-        onPlayerInteract(e) {
-            const { block, player } = e;
-            if(Math.floor(Math.random() * 100) > 20) return;
-
-            const equipment = player.getComponent('equippable');
-            const selectedItem = equipment.getEquipment('Mainhand');
-            if (!selectedItem || selectedItem.typeId != "minecraft:bone_meal") return;
-            
-            const blockdAndFaces = [{block:block.above(), face:"up"},{block:block.below(), face:"down"},{block:block.north(), face:"north"}, 
-                                    {block:block.south(), face:"south"},{block:block.west(), face:"west"},{block:block.east(), face:"east"},]
-            //ive used "block" enough times. Lets spice things up                        
-            const availableLumps = blockdAndFaces.filter(parallelepiped => 
-                                                            parallelepiped.block.isAir || parallelepiped.block.typeId === "ps:cluster_glowstone");
-
-            if(availableLumps.length === 0) return;
-
-            const budToGrow = availableLumps[Math.floor(Math.random() * availableLumps.length)]
-            
-            if(budToGrow.block.isAir){
-                budToGrow.block.setType("ps:cluster_glowstone")
-                budToGrow.block.setPermutation(budToGrow.block.permutation.withState("minecraft:block_face", budToGrow.face)); 
-            }
-            else{
-                const seedStage = budToGrow.block.permutation.getState('ps:crystal_stage');
-                if(seedStage === 4) return;
-                budToGrow.block.setPermutation(budToGrow.block.permutation.withState('ps:crystal_stage', seedStage+1)); 
-            }
-        }
-    });
-});
-
-world.afterEvents.playerBreakBlock.subscribe((e) => {
-    const { brokenBlockPermutation, dimension,block } = e;
-
-    if (brokenBlockPermutation.type.id === "minecraft:glowstone" && (Math.floor(Math.random() * 100) < 5)) {
-        dimension.spawnItem(new ItemStack("ps:glowstone_shard", 1), block.location);
-    }
-});
-
-world.beforeEvents.worldInitialize.subscribe(eventData => {
-    eventData.blockComponentRegistry.registerCustomComponent('ps:bop_cluster_glowstone', {
-        beforeOnPlayerPlace(e) {
-            const { player, block, face, permutationToPlace, dimension } = e;
-            const affectedBlock = getBlockFromFace(block, face)
-            if(!affectedBlock.isSolid){
-                e.cancel = true;
-                return;
-            }
-            const equipment = player.getComponent('equippable');
-            const selectedItem = equipment.getEquipment('Mainhand');
-            let stageSize; 
-            
-            if(selectedItem.typeId === "ps:glowstone_cluster"){
-                stageSize = 4;
-            }else{
-                const clusterSize = selectedItem.typeId.split("_")[0].substring(3);
-
-                switch(clusterSize){
-                    case "large":
-                        stageSize = 3;
-                    break;
-                    case "medium":
-                        stageSize = 2;
-                    break;
-                    case "small":
-                        stageSize = 1;
-                    break;
-                }
-            }
-            e.permutationToPlace = permutationToPlace.withState('ps:crystal_stage', stageSize);
-            }
-    });
-});
+system.runInterval(
+    () => {
+        const players = world.getAllPlayers();
+        for (let i = 0; i < players.length; i++) {
+            const player = players[i];
+            try {
+                const { block, face } = player.getBlockFromViewDirection();
+                if (!block) {
+                    player.onScreenDisplay.setActionBar( "Not looking at a Block." );
+                    return;
+                };
+                
+                player.onScreenDisplay.setActionBar(
+                    `§rblock: §7${block.typeId}§r, face: §7${face}§r, xyz: §6${block.location.x} §r/ §6${block.location.y} §r/ §6${block.location.z}§r,\n`
+                    + `data: §7${JSON.stringify(block.permutation.getAllStates(), null, 4)}`
+                );
+            } catch {
+                player.onScreenDisplay.setActionBar( "Not looking at a Block." );
+            };
+        };
+    },
+);
