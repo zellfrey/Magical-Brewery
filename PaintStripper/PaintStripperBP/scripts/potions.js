90105c9bb3b13311306f8e1014107ba51bd42385
Beardedflea <Linzell.Godfrey@live.co.uk>
2024-10-27 14:00:04 +0000

    wee test of potions

---
+++ PaintStripper/PaintStripperBP/scripts/potions.js
@@ -0,0 +1,14 @@
+import {world, ItemStack} from '@minecraft/server';
+import {setMainHand} from './containerUtils.js';
+
+//12 mins duration in ticks i.e 12*60 *20
+const xLongDuration = 14400;
+world.beforeEvents.worldInitialize.subscribe(eventData => {
+    eventData.itemComponentRegistry.registerCustomComponent('ps:oc_potion', {
+        onConsume(e) {
+            const { source, itemStack} = e;
+            source.addEffect(itemStack.getTags()[0], xLongDuration, { amplifier: 0 })
+            
+        }
+    });
+});
