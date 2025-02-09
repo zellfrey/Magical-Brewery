3a6709227948981b96a684a8f9c768c92d43c1fe
Beardedflea <Linzell.Godfrey@live.co.uk>
2025-02-09 10:43:40 +0000

    moved functions

--- PaintStripper/PaintStripperBP/scripts/potions.js
+++ PaintStripper/PaintStripperBP/scripts/potions.js
@@ -12,3 +12,37 @@ world.beforeEvents.worldInitialize.subscribe(eventData => {
         }
     });
 });
+
+world.afterEvents.itemCompleteUse.subscribe((e) => {
+    const { source, itemStack } = e;
+    if(itemStack.typeId !== "minecraft:potion") return;
+
+    const potion = itemStack.getComponent('minecraft:potion')
+    const lore = itemStack.getLore();
+    if(lore.length != 0){
+        console.log(lore.length)
+        lore.forEach(modifier => {
+            const words = modifier.split(' ');
+            words.pop();
+            let effect = words.join("_").toLowerCase()
+            // if(Potions.getPotionEffectType(effect) ! == undefined){
+            // }
+            console.log(effect)
+            if(effect === "instant_health" || effect === "instant_damage"){
+                source.addEffect(effect, 1, { amplifier: 0 })
+           }else{
+               source.addEffect(effect, 2400, { amplifier: 0 })
+           } 
+        });
+    }
+});
+
+world.afterEvents.entitySpawn.subscribe((e) => {
+    const {entity} = e;
+    // const item = entity.getComponent("item").itemStack;
+    if(entity.typeId !== "minecraft:splash_potion") return;
+
+    console.warn(entity.typeId)
+    
+
+});
