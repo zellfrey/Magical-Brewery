28b8b4de1af8648fe6961b96d6c656c2a65f7be7
Beardedflea <Linzell.Godfrey@live.co.uk>
2025-07-14 19:13:11 +0100

    Starting slow with refactoring, as a mass refactoring made the addon shit the bed.

--- PaintStripper/PaintStripperBP/scripts/tools/agelessPocketWatch.js
+++ PaintStripper/PaintStripperBP/scripts/tools/agelessPocketWatch.js
@@ -1,6 +1,6 @@
 import {world, system, ItemStack, MolangVariableMap } from "@minecraft/server";
 import {crystalGrowth} from "crystal/growingCrystal.js";
-import {getSurroundingBlocks, growCrystalBud} from "crystal/buddingCrystal.js";
+import {getSurroundingBlocks, growCrystalBud} from "crystal/BuddingCrystal.js";
 import {shouldCaskAge, setPotionEffectForCask} from "cask/cask.js";
 import {findCask} from "cask/caskDB.js";
 
@@ -17,60 +17,49 @@ system.beforeEvents.startup.subscribe(eventData => {
                 return;
             }
                 
-
             if(block.typeId === "magical_brewery:growing_crystal"){
-                const seedStage = block.permutation.getState('magical_brewery:crystal_stage');
-                const rotation = Math.floor(Math.random() * 4)
-                block.setPermutation(block.permutation.withState("magical_brewery:crystal_rotation", rotation));
-                crystalGrowth(block, seedStage)
+                forceGrowGrowingCrystal(block)
                 // source.sendMessage("The crystal creaks as it grows from the passage of time") 
 
             }else if(buddingCrystals.includes(block.typeId)){
-                const buddingCrystal = buddingCrystals.find(bud => bud === block.typeId)
-
-                switch(buddingCrystal){
-                    case "magical_brewery:budding_glowstone":
-                        if(block.dimension.id !== "minecraft:nether") return;
-                        forceGrowCrystal(block, "magical_brewery:glowstone_bud", "glowstone", -14)
-                    break;
-                    case "magical_brewery:budding_redstone":
-                        forceGrowCrystal(block, "magical_brewery:redstone_bud", "redstone", -13)
-                    break;
-                    case "magical_brewery:budding_pure_quartz":
-                        forceGrowCrystal(block, "magical_brewery:pure_quartz_bud", "pure_quartz", -16)
-                    break;
-                    case "magical_brewery:budding_echo":
-                        forceGrowCrystal(block, "magical_brewery:echo_bud", "echo", -9)
-                    break;
-                }
+                forceGrowBuddingCrystal(block)   
                 // source.sendMessage("Sparks fly out of the budding crystal as its forced to grow shards.") 
 
             }else if(block.typeId.includes("magical_brewery:cask")){
-                const {x,y,z} = block.location;
-                let cask = findCask(block.dimension.id, {x, y, z})
-                const fillLevel = block.permutation.getState("magical_brewery:fill_level");
-                const caskEffect = block.getComponent("magical_brewery:pi_cask_fill").customComponentParameters.params.cask_effect
-                const canAge = shouldCaskAge(caskEffect, cask.potion_effects)
-                const aged = block.permutation.getState("magical_brewery:aged");
-                
-
-                if(!canAge || aged || fillLevel === 0){
-                    source.sendMessage("Despite accelerating time, the cask cannot age.")
-                    return;
-                } 
-                const particleLocation = block.center();
-                particleLocation.y += 0.4
-                block.dimension.spawnParticle("minecraft:crop_growth_emitter", particleLocation);
-                block.setPermutation(block.permutation.withState("magical_brewery:aged", true));
-                setPotionEffectForCask(caskEffect, cask, 100)
-                source.sendMessage("The watch has sped up time, and has aged the cask.")
+                forceAgeCask(block, source)
             }
             block.dimension.playSound("conduit.ambient", block.location, {volume: 0.8, pitch: 3});
         }
     });
 });
 
-function forceGrowCrystal(block, budTag, type, CharNum){
+function forceGrowGrowingCrystal(block){
+    const seedStage = block.permutation.getState('magical_brewery:crystal_stage');
+    const rotation = Math.floor(Math.random() * 4)
+    block.setPermutation(block.permutation.withState("magical_brewery:crystal_rotation", rotation));
+    crystalGrowth(block, seedStage)
+}
+
+function forceGrowBuddingCrystal(block){
+    const buddingCrystal = buddingCrystals.find(bud => bud === block.typeId)
+
+    switch(buddingCrystal){
+        case "magical_brewery:budding_glowstone":
+            if(block.dimension.id !== "minecraft:nether") return;
+            checkValidCrystalGrowth(block, "magical_brewery:glowstone_bud", "glowstone", -14)
+        break;
+        case "magical_brewery:budding_redstone":
+            checkValidCrystalGrowth(block, "magical_brewery:redstone_bud", "redstone", -13)
+        break;
+        case "magical_brewery:budding_pure_quartz":
+            checkValidCrystalGrowth(block, "magical_brewery:pure_quartz_bud", "pure_quartz", -16)
+        break;
+        case "magical_brewery:budding_echo":
+            checkValidCrystalGrowth(block, "magical_brewery:echo_bud", "echo", -9)
+        break;
+    }
+}
+function checkValidCrystalGrowth(block, budTag, type, CharNum){
     const validBlocks = getSurroundingBlocks(block, budTag)
 
     if(validBlocks.length === 0) return;
@@ -78,4 +67,25 @@ function forceGrowCrystal(block, budTag, type, CharNum){
     const budToGrow = validBlocks[Math.floor(Math.random() * validBlocks.length)]
 
     growCrystalBud(budToGrow, type, CharNum)
+}
+
+function forceAgeCask(block, source){
+    const {x,y,z} = block.location;
+    let cask = findCask(block.dimension.id, {x, y, z})
+    const fillLevel = block.permutation.getState("magical_brewery:fill_level");
+    const caskEffect = block.getComponent("magical_brewery:pi_cask_fill").customComponentParameters.params.cask_effect
+    const canAge = shouldCaskAge(caskEffect, cask.potion_effects)
+    const aged = block.permutation.getState("magical_brewery:aged");
+    
+
+    if(!canAge || aged || fillLevel === 0){
+        source.sendMessage("Despite accelerating time, the cask cannot age.")
+        return;
+    } 
+    const particleLocation = block.center();
+    particleLocation.y += 0.4
+    block.dimension.spawnParticle("minecraft:crop_growth_emitter", particleLocation);
+    block.setPermutation(block.permutation.withState("magical_brewery:aged", true));
+    setPotionEffectForCask(caskEffect, cask, 100)
+    source.sendMessage("The watch has sped up time, and has aged the cask.")
 }
--- PaintStripper/PaintStripperBP/scripts/tools/glassmithHammer.js
+++ PaintStripper/PaintStripperBP/scripts/tools/glassmithHammer.js
@@ -5,21 +5,25 @@ system.beforeEvents.startup.subscribe(eventData => {
     eventData.itemComponentRegistry.registerCustomComponent('magical_brewery:on_use_on_hammer', {
         onUseOn(e) {
             const {block, itemStack, source} = e;
-            if(!block.hasTag("magical_brewery:crystal_seed")) return;
-
-            const seedDepth = block.permutation.getState("magical_brewery:seed_depth")
-            if(seedDepth != 4){
-                block.setPermutation(block.permutation.withState("magical_brewery:seed_depth", seedDepth+1));
-                block.dimension.spawnParticle("minecraft:critical_hit_emitter", block.center()); 
-                const pitch = seedDepth * 0.75
-                block.dimension.playSound("hit.amethyst_block", block.location, {volume: 0.8, pitch: pitch});
-
-                setItemStackDurability(itemStack, source)
-            }     
+            if(block.hasTag("magical_brewery:crystal_seed")){
+                setCrystalSeedDepth(block, itemStack, source)   
+            }
+            
         }
     });
 });
 
+function setCrystalSeedDepth(block, itemStack, source){
+    const seedDepth = block.permutation.getState("magical_brewery:seed_depth")
+    if(seedDepth != 4){
+        block.setPermutation(block.permutation.withState("magical_brewery:seed_depth", seedDepth+1));
+        block.dimension.spawnParticle("minecraft:critical_hit_emitter", block.center()); 
+        const pitch = seedDepth * 0.75
+        block.dimension.playSound("hit.amethyst_block", block.location, {volume: 0.8, pitch: pitch});
+
+        setItemStackDurability(itemStack, source)
+    }
+}
 function setItemStackDurability(itemStack, player){
     const enchants = itemStack.getComponent("minecraft:enchantable");
     const unbreakingLevel = enchants?.getEnchantment("unbreaking")?.level ?? 0;
