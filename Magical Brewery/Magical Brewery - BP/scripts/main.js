5b3000fa993a43a95f4d399ab4d0ec31505f62cf
Beardedflea <Linzell.Godfrey@live.co.uk>
2025-09-23 14:20:00 +0100

    Cleaned sinbin folder. Stuff may not be appropriate. A more strict pass on WIP items

--- Magical Brewery/Magical Brewery - BP/scripts/main.js
+++ Magical Brewery/Magical Brewery - BP/scripts/main.js
@@ -1,6 +1,3 @@
-import {world, system, ItemStack} from "@minecraft/server";
-import {getAdjacentBlock, getBlockFromFace} from "./utils/blockPlacementUtils.js";
-import {setMainHand} from './utils/containerUtils.js';
 import "./crystal/BuddingCrystal.js"
 import "./crystal/growingCrystal.js"
 import "./crystal/SiftedDust.js"
@@ -13,27 +10,3 @@ import "./cask/Seal.js"
 import "./potionEffects.js"
 import "./tome/tome.js"
 import "./utils/lootUtils.js"
-
-
-system.runInterval(
-    () => {
-        const players = world.getAllPlayers();
-        for (let i = 0; i < players.length; i++) {
-            const player = players[i];
-            try {
-                const { block, face } = player.getBlockFromViewDirection();
-                if (!block) {
-                    player.onScreenDisplay.setActionBar( "Not looking at a Block." );
-                    return;
-                };
-                
-                player.onScreenDisplay.setActionBar(
-                    `§rblock: §7${block.typeId}§r, face: §7${face}§r, xyz: §6${block.location.x} §r/ §6${block.location.y} §r/ §6${block.location.z}§r,\n`
-                    + `data: §7${JSON.stringify(block.permutation.getAllStates(), null, 4)}`
-                );
-            } catch {
-                player.onScreenDisplay.setActionBar( "Not looking at a Block." );
-            };
-        };
-    },
-);
