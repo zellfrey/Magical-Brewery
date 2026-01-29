c11aad7a4e45b1702d19edbb31f80c8d8f075ffa
Beardedflea <Linzell.Godfrey@live.co.uk>
2026-01-29 15:58:27 +0000

    Removed desk item/block for the time being

--- Magical Brewery/Magical Brewery - BP/scripts/main.js
+++ Magical Brewery/Magical Brewery - BP/scripts/main.js
@@ -12,28 +12,28 @@ import "./tome/tome.js"
 import "./utils/lootUtils.js"
 import "./brewersDesk.js"
 
-import {world, system, BlockPermutation} from "@minecraft/server";
+// import {world, system, BlockPermutation} from "@minecraft/server";
 
-system.runInterval(
-    () => {
-        const players = world.getAllPlayers();
-        for (let i = 0; i < players.length; i++) {
-            const player = players[i];
-            try {
-                const { block, face } = player.getBlockFromViewDirection();
-                if (!block) {
-                    player.onScreenDisplay.setActionBar( "Not looking at a Block." );
-                    return;
-                };
+// system.runInterval(
+//     () => {
+//         const players = world.getAllPlayers();
+//         for (let i = 0; i < players.length; i++) {
+//             const player = players[i];
+//             try {
+//                 const { block, face } = player.getBlockFromViewDirection();
+//                 if (!block) {
+//                     player.onScreenDisplay.setActionBar( "Not looking at a Block." );
+//                     return;
+//                 };
                 
-                player.onScreenDisplay.setActionBar(
-                    `§rblock: §7${block.typeId}§r, face: §7${face}§r, xyz: §6${block.location.x} §r/ §6${block.location.y} §r/ §6${block.location.z}§r,\n`
-                    + `data: §7${JSON.stringify(block.permutation.getAllStates(), null, 4)}`
-                );
-            } catch {
-                player.onScreenDisplay.setActionBar( "Not looking at a Block." );
-            };
-        };
-    },
-);
+//                 player.onScreenDisplay.setActionBar(
+//                     `§rblock: §7${block.typeId}§r, face: §7${face}§r, xyz: §6${block.location.x} §r/ §6${block.location.y} §r/ §6${block.location.z}§r,\n`
+//                     + `data: §7${JSON.stringify(block.permutation.getAllStates(), null, 4)}`
+//                 );
+//             } catch {
+//                 player.onScreenDisplay.setActionBar( "Not looking at a Block." );
+//             };
+//         };
+//     },
+// );
 
