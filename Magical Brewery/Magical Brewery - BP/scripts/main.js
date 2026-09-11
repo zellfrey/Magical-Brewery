011e5bda2c1d334af97cca2a292566d6ffb3c12c
Beardedflea <Linzell.Godfrey@live.co.uk>
2026-09-11 01:30:56 +0100

    0.5 changelog

--- Magical Brewery/Magical Brewery - BP/scripts/main.js
+++ Magical Brewery/Magical Brewery - BP/scripts/main.js
@@ -14,31 +14,31 @@ import "./tome/Tome.js"
 import "./tome/TomeResearch.js"
 import "./tome/pages.js"
 import "./utils/lootUtils.js"
-import "./utils/commandUtils.js"
+//import "./utils/commandUtils.js"
 import "./brewersDesk.js"
 //import "./cracklingOre.js"
 
 import {world, system, BlockPermutation} from "@minecraft/server";
 
-system.runInterval(
-    () => {
-        const players = world.getAllPlayers();
-        for (let i = 0; i < players.length; i++) {
-            const player = players[i];
-            try {
-                const { block, face } = player.getBlockFromViewDirection();
-                if (!block) {
-                    player.onScreenDisplay.setActionBar( "Not looking at a Block." );
-                    return;
-                };
+// system.runInterval(
+//     () => {
+//         const players = world.getAllPlayers();
+//         for (let i = 0; i < players.length; i++) {
+//             const player = players[i];
+//             try {
+//                 const { block, face } = player.getBlockFromViewDirection();
+//                 if (!block) {
+//                     player.onScreenDisplay.setActionBar( "Not looking at a Block." );
+//                     return;
+//                 };
                 
-                player.onScreenDisplay.setActionBar(
-                    `§rblock: §7${block.typeId}§r, face: §7${face}§r, xyz: §6${block.location.x} §r/ §6${block.location.y} §r/ §6${block.location.z}§r,\n`
-                    + `data: §7${JSON.stringify(block.permutation.getAllStates(), null, 4)}`
-                );
-            } catch {
-                player.onScreenDisplay.setActionBar( "Not looking at a Block." );
-            };
-        };
-    },
-);
+//                 player.onScreenDisplay.setActionBar(
+//                     `§rblock: §7${block.typeId}§r, face: §7${face}§r, xyz: §6${block.location.x} §r/ §6${block.location.y} §r/ §6${block.location.z}§r,\n`
+//                     + `data: §7${JSON.stringify(block.permutation.getAllStates(), null, 4)}`
+//                 );
+//             } catch {
+//                 player.onScreenDisplay.setActionBar( "Not looking at a Block." );
+//             };
+//         };
+//     },
+// );
--- Magical Brewery/Magical Brewery - BP/scripts/utils/commandUtils.js
+++ Magical Brewery/Magical Brewery - BP/scripts/utils/commandUtils.js
@@ -1,39 +1,41 @@
-import {CommandPermissionLevel, CustomCommandParamType, CustomCommandStatus, system, world, Player} from "@minecraft/server";
+// import {CommandPermissionLevel, CustomCommandParamType, CustomCommandStatus, system, world, Player} from "@minecraft/server";
 
-system.beforeEvents.startup.subscribe(({ customCommandRegistry }) => {
-	customCommandRegistry.registerCommand(
-		{
-			name: "magical_brewery:resetplayertomedata",
-			description: "Dev command to reset player tome data.",
-			//description: "Dev command to reset player tome data. The affected player must be online",
-			permissionLevel: CommandPermissionLevel.Admin,
-			cheatsRequired: false,
-			mandatoryParameters: [
-				{ name: "player", type: CustomCommandParamType.PlayerSelector  },
-				{ name: "confirmation", type: CustomCommandParamType.Boolean },
-			],
-		},
-		(origin, player, confirmation) => {
+
+// //DEV COMMAND. Honestly couldnt get this to properly work so it's only use is testing in singleplayer. I'll probably come back to this
+// system.beforeEvents.startup.subscribe(({ customCommandRegistry }) => {
+// 	customCommandRegistry.registerCommand(
+// 		{
+// 			name: "magical_brewery:resetplayertomedata",
+// 			description: "Dev command to reset player tome data.",
+// 			//description: "Dev command to reset player tome data. The affected player must be online",
+// 			permissionLevel: CommandPermissionLevel.Admin,
+// 			cheatsRequired: false,
+// 			mandatoryParameters: [
+// 				{ name: "player", type: CustomCommandParamType.PlayerSelector  },
+// 				{ name: "confirmation", type: CustomCommandParamType.Boolean },
+// 			],
+// 		},
+// 		(origin, player, confirmation) => {
 			
-			const targetPlayer = world.getAllPlayers()[0];
-			//const targetPlayer = world.getAllPlayers().filter(el => el.id === player["0"].id)
-			//if(!(targetPlayer instanceof Player)) console.log("false")
-			if(!confirmation){
-				return {
-                    status: CustomCommandStatus.Failure,
-                    message: "Confirmation denied",
-                };
-			}
+// 			const targetPlayer = world.getAllPlayers()[0];
+// 			//const targetPlayer = world.getAllPlayers().filter(el => el.id === player["0"].id)
+// 			//if(!(targetPlayer instanceof Player)) console.log("false")
+// 			if(!confirmation){
+// 				return {
+//                     status: CustomCommandStatus.Failure,
+//                     message: "Confirmation denied",
+//                 };
+// 			}
 			
-			system.run(() => targetPlayer.clearDynamicProperties());
-			return {
-                status: CustomCommandStatus.Success,
-                message: "Successfully reset player tome data",
-            };
-		}
-	);
-});
+// 			system.run(() => targetPlayer.clearDynamicProperties());
+// 			return {
+//                 status: CustomCommandStatus.Success,
+//                 message: "Successfully reset player tome data",
+//             };
+// 		}
+// 	);
+// });
 
-// function commandResetPlayerTomeData(customCommandRegistry){
+// // function commandResetPlayerTomeData(customCommandRegistry){
 
-// }
+// // }
